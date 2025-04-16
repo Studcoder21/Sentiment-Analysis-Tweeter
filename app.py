7801242a1a95025d358473def6be9d6ec329ed35
Studcoder21 <160550518+Studcoder21@users.noreply.github.com>
2025-04-16 19:14:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -392,7 +392,7 @@ def convert_emojis_to_text(text):
 def load_model():
     # Baixar modelo do Google Drive
     model_url = "https://drive.google.com/uc?id=1BUh-SLS8_8H5V4ID4sm6T0kY4h-KNaH8"
-    gdown.download(model_url, model_zip, quiet=False)
+    gdown.download(model_url, final_model, quiet=False)
     
     
     # Carregar modelo
